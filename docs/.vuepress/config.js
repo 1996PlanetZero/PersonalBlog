a3c78c261d174cd2cbb7bdb1e146737c43e1abfb
zlp <lpzhu96@qq.com>
2024-06-24 21:59:12 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: "Zlp Space",
+    description: "Remember all the sparkling points in your life",
+    markdown: {
+        lineNumbers: true,
+    },
+    themeConfig: {
+        sidebar: [
+            {
+                title: 'Tech Sharing',
+                children: ['/Tech Sharing/玩转WSL' ],
+                initialOpenGroupIndex: 1 // 可选的, 默认值是 0
+            }
+        ]
+    },
+};
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroText: Zlp Space
+tagline: 万类霜天竞自由
+actionText: 探索
+actionLink: /Tech Sharing/玩转WSL
+features:
+- title: Tech Sharing
+  details: 技术分享
+- title: Sparkling Thoughts
+  details: 人生感悟
+  footer: Welcome to Zlp Space!
+
+---
